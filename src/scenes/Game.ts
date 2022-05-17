b7886492fd0a2b44f09be22ea95e0e4ce3a69466
GolfGrab <surapus01@gmail.com>
2022-05-17 21:20:06 +0700

    add idle and walk animation

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,73 @@
-import Phaser from 'phaser';
+import Phaser from "phaser"
 
-export default class Demo extends Phaser.Scene {
+export default class Game extends Phaser.Scene {
+
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
+  
+  private penguin!: Phaser.Physics.Matter.Sprite
+  
   constructor() {
-    super('GameScene');
+    super("GameScene")
+  }
+
+  init()
+  {
+    this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.atlas("penguin", "assets/penguin.png", "assets/penguin.json")
+    this.load.image("tiles", "assets/sheet.png")
+    this.load.tilemapTiledJSON("tilemap", "assets/game.json")
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
+    this.createPlayerAnimation()
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
+    const { width, height } = this.scale
+
+    const map = this.make.tilemap({ key: "tilemap" })
+    const tileset = map.addTilesetImage("iceworld", "tiles")
+
+    const ground = map.createLayer("ground", tileset)
+    ground.setCollisionByProperty({ collides: true })
+
+    this.matter.world.convertTilemapLayer(ground)
+
+
+    this.penguin = this.matter.add.sprite(width / 2, height / 2, "penguin")
+      .play("player-idle")
+  }
+
+  update() {
+    if(this.cursors.left.isDown) {
+    }
+  }
+
+  private createPlayerAnimation() {
+
+    this.anims.create({
+      key: "player-idle",
+      frames: [{
+        key: "penguin",
+        frame: "penguin_walk01.png"
+      }],
+    })
+
+
+    this.anims.create({
+      key: "player-walk",
+      frameRate: 10,
+      frames: this.anims.generateFrameNames("penguin", {
+        start: 1,
+        end: 4,
+        zeroPad: 2,
+        prefix: "penguin_walk",
+        suffix: ".png"
+      }),
       repeat: -1
-    });
+    })
+
+
   }
 }
